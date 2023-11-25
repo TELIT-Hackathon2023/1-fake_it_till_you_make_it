011d0cfc170a6425ffde26bf9db71d5f7c50b88d
dostavic <dmytro.havrysh@outlook.com>
2023-11-25 19:29:40 +0100

    screenshot vision

--- ai.py
+++ ai.py
@@ -1,42 +1,64 @@
+# Python program to take
+# screenshots 
+
+
+import numpy as np
+import cv2
+import pyautogui
 import base64
+import requests
 
-import openai
+image = pyautogui.screenshot()
 
+
+image = cv2.cvtColor(np.array(image),
+                     cv2.COLOR_RGB2BGR)
+cv2.imwrite("image1.png", image)
+
+# writing it to the disk using opencv 
+#cv2.imwrite("image1.png", image)
+
+api_key  =
+
+# Function to encode the image
 def encode_image(image_path):
   with open(image_path, "rb") as image_file:
     return base64.b64encode(image_file.read()).decode('utf-8')
 
-openai.api_key = ''  # Replace with your actual API key
+# Path to your image
+image_path = "image1.png"
 
-
-image_path = "C:/Users/Vasyl/Pictures/Screenshots/Знімок екрана 2023-04-11 183302.png"
-base64_image = encode_image(image_path)
 # Getting the base64 string
-messages = []
+base64_image = encode_image(image_path)
 
-while True:
-    message = input("User: ")
-    if message and message != "ano":
-        messages.append({"role": "user", "content": message})
-        response = openai.Completion.create(
-            engine="text-davinci-003",  # Use the appropriate engine for your needs
-            prompt=" ".join([m['content'] for m in messages]),
-            max_tokens=150
-        )
+headers = {
+  "Content-Type": "application/json",
+  "Authorization": f"Bearer {api_key}"
+}
 
-        reply = response['choices'][0]['text']
-        print(f"ChatGPT: {reply}")
-        messages.append({"role": "assistant", "content": reply})
-    else:
-        message = base64_image
-        messages.append({"role": "user", "content": message})
-        response = openai.Completion.create(
-            engine="text-davinci-003",  # Use the appropriate engine for your needs
-            prompt=" ".join([m['content'] for m in messages]),
-            max_tokens=150
-        )
+payload = {
+  "model": "gpt-4-vision-preview",
+  "messages": [
+    {
+      "role": "user",
+      "content": [
+        {
+          "type": "text",
+          "text": "what do you see in this image?"
+        },
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": f"data:image/jpeg;base64,{image}"
+          }
+        }
+      ]
+    }
+  ],
+  "max_tokens": 300
+}
 
-        reply = response['choices'][0]['text']
-        print(f"ChatGPT: {reply}")
-        messages.append({"role": "assistant", "content": reply})
+response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+print(response.json())
 
