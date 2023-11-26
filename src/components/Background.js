b6c2f14d211dbcc2cc313568c8041b2491af56ea
Maksym Furmanov <maksym.furmanov@student.tuke.sk>
2023-11-26 08:45:43 +0100

    Entery page full done

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,33 +1,56 @@
-import logo from "../assets/images/LOGO.svg"
-import vector1 from "../assets/images/Background_svg/Vector 1.svg"
-import vector2 from "../assets/images/Background_svg/Vector 2.svg"
-import vector3 from "../assets/images/Background_svg/Vector 3.svg"
-import vector4 from "../assets/images/Background_svg/Vector 4.svg"
-import vector6 from "../assets/images/Background_svg/Vector 6.svg"
-import vector7 from "../assets/images/Background_svg/Vector 7.svg"
-
+import React, {useState} from 'react';
+import logo from "../assets/images/LOGO.svg";
+import vector1 from "../assets/images/Background_svg/Vector 1.svg";
+import vector2 from "../assets/images/Background_svg/Vector 2.svg";
+import vector3 from "../assets/images/Background_svg/Vector 3.svg";
+import vector4 from "../assets/images/Background_svg/Vector 4.svg";
+import vector6 from "../assets/images/Background_svg/Vector 6.svg";
+import vector7 from "../assets/images/Background_svg/Vector 7.svg";
 
 const Background = ({children}) => {
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
+
     return (
         <>
-            {children}
-            <div className={"Background"}>
-                <div className={"svg-vertical-group"}>
-                    <img src={logo} alt={"UXplorer"} className={"logo"}/>
-                    <img src={vector1} alt={""} className={"bg-vector-1"}/>
-                    <img src={vector2} alt={""} className={"bg-vector-2"}/>
-                    <img src={vector3} alt={""} className={"bg-vector-3"}/>
+            <div className={"block-bg"}>
+                <div
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                >
+                    {children}
                 </div>
-                <div className={"svg-vertical-group"}>
-                    <img src={vector4} alt={""} className={"bg-vector-4"}/>
+            </div>
+            <div className="Background">
+                <div className="svg-vertical-group">
+                    <img src={logo} alt="UXplorer" className="logo"/>
+                    <img style={{filter: isHovered ? 'none' : 'blur(3px)'}}
+                         src={vector1} alt="" className="bg-vector-1"/>
+                    <img style={{filter: isHovered ? 'none' : 'blur(3px)'}}
+                         src={vector2} alt="" className="bg-vector-2"/>
+                    <img style={{filter: isHovered ? 'none' : 'blur(3px)'}}
+                         src={vector3} alt="" className="bg-vector-3"/>
                 </div>
-                <div className={"svg-vertical-group"}>
-                    <img src={vector6} alt={""} className={"bg-vector-6"}/>
-                    <img src={vector7} alt={""} className={"bg-vector-7"}/>
+                <div className="svg-vertical-group">
+                    <img style={{filter: isHovered ? 'none' : 'blur(3px)'}}
+                         src={vector4} alt="" className="bg-vector-4"/>
+                </div>
+                <div className="svg-vertical-group">
+                    <img style={{filter: isHovered ? 'none' : 'blur(3px)'}}
+                         src={vector6} alt="" className="bg-vector-6"/>
+                    <img style={{filter: isHovered ? 'none' : 'blur(3px)'}}
+                         src={vector7} alt="" className="bg-vector-7"/>
                 </div>
             </div>
         </>
     )
-}
+};
 
 export default Background;
--- src/components/WebsiteForm.js
+++ src/components/WebsiteForm.js
@@ -5,30 +5,49 @@ import {useState} from "react";
 const WebsiteForm = () => {
     const navigate = useNavigate();
     const setUrl = useSetUrl();
-    const [urlInput, setUrlInput] = useState("");
+    const [inputs, setInputs] = useState({
+        url: "",
+        gitUrl: "",
+        comment: ""
+    });
 
     const submitHandler = () => {
-        setUrl(urlInput);
+        setUrl(inputs.url);
         navigate("/scenarios");
     }
 
-    const inputHandler = (e) => {
-        setUrlInput(e.target.value);
-    }
+    const inputHandler = (e, input) => {
+        setInputs(prevState => ({
+            ...prevState,
+            [input]: e.target.value
+        }));
+    };
 
     return (
-        <div className={"WebsiteForm-background"}>
             <form className={"WebsiteForm"} onSubmit={submitHandler}>
                 <h1>Start testing your website</h1>
-                <input onChange={(e) => inputHandler(e)}
-                       value={urlInput}
+                <input onChange={(e) => inputHandler(e, "url")}
+                       value={inputs.url}
                        type={"text"}
-                       placeholder={"Your URL"}
+                       placeholder={"URL-address your website"}
+                       name={"url"}
+                       required={true}/>
+                <input onChange={(e) => inputHandler(e, "gitUrl")}
+                       value={inputs.gitUrl}
+                       type={"text"}
+                       placeholder={"URL-address your website (optional)"}
                        name={"url"}/>
-                <textarea rows={5} placeholder={"Shortly about project"} name={"comment"}/>
-                <button>Analyze</button>
+                <div className={"add-files"}><button>+ Add files</button></div>
+                <textarea onChange={(e) => inputHandler(e, "comment")}
+                          value={inputs.comment}
+                          rows={5}
+                          placeholder={"Shortly about project"}
+                          name={"comment"}
+                          required={true}/>
+                <div className={"start"}>
+                    <button>START</button>
+                </div>
             </form>
-        </div>
     );
 }
 
